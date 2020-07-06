29b1bc72a22606379c3bf4c8bc045cdcefc5ff76
Silvia Eveline <evelineborges1992@gmail.com>
2020-07-05 21:35:06 -0300

    Salvando no storage

--- todos.js
+++ todos.js
@@ -2,11 +2,7 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-    'Fazer café',
-    'Estudar JavaScript',
-    'Acessar comunidade da Rocketseat'
-];
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
 function renderTodos() {
 
@@ -22,7 +18,7 @@ function renderTodos() {
         linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
 
         var linkText = document.createTextNode('Excluir');
-       
+
         linkElement.appendChild(linkText);
         todoElement.appendChild(todoText);
         todoElement.appendChild(linkElement);
@@ -38,6 +34,7 @@ function addTodo() {
     todos.push(todoText);
     inputElement.value = '';
     renderTodos();
+    saveToStorage();
 }
 
 buttonElement.onclick = addTodo;
@@ -46,4 +43,9 @@ buttonElement.onclick = addTodo;
 function deleteTodo(pos) {
     todos.splice(pos, 1);
     renderTodos();
+    saveToStorage();
+}
+
+function saveToStorage(){
+    localStorage.setItem('list_todos', JSON.stringify(todos));
 }
